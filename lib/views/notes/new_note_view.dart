eea81b8b7ca55bb957d65490e465d088029b268e
Tomek <twesolowski61@gmail.com>
2022-11-13 14:59:24 +0100

    Adjust the type for snapshot.data

--- lib/views/notes/new_note_view.dart
+++ lib/views/notes/new_note_view.dart
@@ -86,7 +86,7 @@ class _NewNoteViewState extends State<NewNoteView> {
         builder: (BuildContext context, AsyncSnapshot snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-              _note = snapshot.data as DatabaseNote?;
+              _note = snapshot.data as DatabaseNote;
               _setupTextControllerListener();
               return TextField(
                 controller: _textController,

facb52108cc7bd0c4348df28f36030d361f20d08
Tomek <twesolowski61@gmail.com>
2022-11-17 21:01:44 +0100

    Require user id in AuthUser

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -3,14 +3,17 @@ import 'package:flutter/material.dart';
 
 @immutable
 class AuthUser {
+  final String id;
   final bool isEmailVerified;
   final String? email;
   const AuthUser({
+    required this.id,
     required this.email,
     required this.isEmailVerified,
   });
 
   factory AuthUser.fromFirebase(User user) => AuthUser(
+        id: user.uid,
         email: user.email,
         isEmailVerified: user.emailVerified,
       );
